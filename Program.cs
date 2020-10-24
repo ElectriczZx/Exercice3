d8d4729a3e850cedf4741f6d8246f08bb231adde
Nico Isbarn <n.isbarn17@gmail.com>
2020-10-24 15:54:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rgen rgen = new Rgen();
+
+            rgen.getRandom();
+        }
+    }
+}
---
+++ Rgen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercice3
+{
+    class Rgen
+    {
+        Random rnd = new Random();
+
+        public void getRandom()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Number: ", rnd.Next(1, 100));
+            }
+        }
+}
+}
